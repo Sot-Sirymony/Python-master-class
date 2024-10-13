28a2f942a3addabd387cc1b2d933d1685cbfa003
Sot Sirymony <sotsirymony@Sots-MacBook-Pro.local>
2024-10-13 19:49:01 +0700

    updated files

---
+++ up-python/operator/SLEEPY.py
@@ -0,0 +1,16 @@
+# import time
+
+# def sleepy_pattern(text):
+#     for char in text:
+#         print(char, end='', flush=True)
+#         time.sleep(0.2)  # Simulate the sleepy, slow typing effect
+#     print()
+
+# # Define your sleepy string
+# sleepy_string = "* z Z Z Z z *"
+
+# # Call the function
+# sleepy_pattern(sleepy_string)
+
+
+
